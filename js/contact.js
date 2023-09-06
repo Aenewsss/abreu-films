1853329d3a1c40c92b8e15912d4a377af4ca1c8d
aenewsss <aenamartinelli@gmail.com>
2023-09-06 08:39:34 -0300

    emialjs

---
+++ js/contact.js
@@ -0,0 +1,33 @@
+const contactName = document.getElementById("contact-name");
+const email = document.getElementById("contact-email");
+const phone = document.getElementById("contact-phone");
+const message = document.getElementById("contact-message");
+
+function handleSubmit(e) {
+    e.preventDefault()
+    const templateParams = {
+        user_name: contactName.value,
+        user_phone: phone.value,
+        user_email: email.value,
+        user_message: message.value
+    }
+
+    const data = {
+        service_id: process.env.NEXT_PUBLIC_SERVICE_ID || "",
+        template_id: process.env.NEXT_PUBLIC_TEMPLATE_ID || "",
+        user_id: process.env.NEXT_PUBLIC_USER_ID || "",
+        template_params: templateParams
+    };
+
+    fetch("https://api.emailjs.com/api/v1.0/email/send", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+        .then(result => console.log(result))
+        .catch(e => console.error(e))
+
+    window.open("https://api.whatsapp.com/send?phone=5561991174617&text=Olá,%20Rodrigo.%20%20%20%20Eu%20gostaria%20de%20saber%20mais%20sobre%20os%20seus%20serviços%20referentes%20a%20criação%20de%20vídeos!", "_blank")
+}
